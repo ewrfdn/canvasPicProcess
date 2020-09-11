6c286dc048da6de26bae25f045fcac6cba0a6d88
ewrfdn <ewrfdn@outlook.com>
2020-09-12 00:14:06 +0800

    模糊算法性能提升

--- src/views/home/homePage.tsx
+++ src/views/home/homePage.tsx
@@ -49,39 +49,150 @@ export default class HomePageComponent extends Component {
   }
   averageBulr(imageData: any, width: number, range: number) {
     let rgbList: number[] = imageData.data
+    console.log(rgbList)
     let imgdata = this.ctx.createImageData(900, 600);
     let resultList = imgdata.data;
     let date = new Date
     width = width * 4
-    let rowLen=~~(rgbList.length/width)
-    let step = range * 4,current=0;
+    let rowLen = ~~(rgbList.length / width)
+    let step = range * 4, current = 0;
     let time1 = date.getTime()
     console.log(rowLen)
-    for (let i = 0; i < rowLen; i ++) {
-      for (let k = 0; k < width; k+=4) {
-          let min=k-step,max=k+step,startRow=i-range,endRow=i+range,R=0,G=0,B=0,A=0,count=0;
-          min=min<0?0:min;
-          max=max>width?width:max
-          startRow=startRow<0?0:startRow
-          endRow=endRow>rowLen?rowLen:endRow
-          for(let r=startRow;r<endRow;r++){
-            for(let c=r*width+min;c<r*width+max;c+=4){
-              count++
-              R+=rgbList[c];
-               G+=rgbList[c+1];
-               B+=rgbList[c+2];
-               A+=rgbList[c+3] ;
-            }
+    for (let i = 0; i < rowLen; i++) {
+      for (let k = 0; k < width; k += 4) {
+        let min = k - step, max = k + step, startRow = i - range, endRow = i + range, R = 0, G = 0, B = 0, A = 0, count = 0;
+        min = min < 0 ? 0 : min;
+        max = max > width ? width : max
+        startRow = startRow < 0 ? 0 : startRow
+        endRow = endRow > rowLen ? rowLen : endRow
+        for (let r = startRow; r < endRow; r++) {
+          for (let c = r * width + min; c < r * width + max; c += 4) {
+            count++
+            R += rgbList[c];
+            G += rgbList[c + 1];
+            B += rgbList[c + 2];
+            A += rgbList[c + 3];
           }
-          resultList[current]=(~~(R/count))
-          resultList[current+1]=(~~(G/count))
-          resultList[current+2]=(~~(B/count))
-          resultList[current+3]=(~~(A/count))
-
-          current+=4
+        }
+        resultList[current] = (~~(R / count))
+        resultList[current + 1] = (~~(G / count))
+        resultList[current + 2] = (~~(B / count))
+        resultList[current + 3] = (~~(A / count))
+        current += 4
       }
     }
     console.log(current)
+    date = new Date
+    let time2 = date.getTime()
+    console.log(`处理耗时${time2 - time1}毫秒`)
+    return imgdata
+
+  }
+  averageBulr1(imageData: any, width: number, range: number) {
+    let rgbList: number[] = imageData.data
+    let imgdata = this.ctx.createImageData(900, 600);
+    let resultList = imgdata.data;
+    let date = new Date
+    width = width * 4
+    let rowLen = ~~(rgbList.length / width)
+    let step = range * 4, current = 0;
+    let steplist:number[]=[], columnList: number[]=[]
+    let time1 = date.getTime()
+    for (let i = 0; i < rowLen; i++) {
+      let startRow = i - range, endRow = i + range
+      startRow = startRow < 0 ? 0 : startRow
+      endRow = endRow > rowLen ? rowLen : endRow
+      if (columnList.length == 0) {
+        for (let q = 0; q < width; q ++) {
+          columnList[q]=0
+          for(let s=startRow;s<endRow;s++){
+            columnList[q]+=rgbList[q+s*width]
+          }
+        }
+      }
+      for (let k = 0; k < width; k += 4) {
+        let min = k - step, max = k + step;
+        min = min < 0 ? 0 : min;
+        max = max > width ? width : max
+       if(k===0){
+         steplist[k]=0
+         steplist[k+1]=0
+         steplist[k+2]=0
+         steplist[k+3]=0
+         for(let x=min;x<max;x+=4){
+           steplist[k]+=columnList[x]
+           steplist[k+1]+=columnList[x+1]
+           steplist[k+2]+=columnList[x+2]
+           steplist[k+3]+=columnList[x+3]
+         }
+       }else{
+         if(max-min===2*step){
+          if(min===0){
+            steplist[k]=steplist[k-4]+columnList[max-4]
+            steplist[k+1]=steplist[k-3]+columnList[max-3]
+            steplist[k+2]=steplist[k-2]+columnList[max-2]
+            steplist[k+3]=steplist[k-1]+columnList[max-1]
+          }else{
+            steplist[k]=steplist[k-4]+columnList[max-4]-columnList[min-4]
+            steplist[k+1]=steplist[k-3]+columnList[max-3]-columnList[min-3]
+            steplist[k+2]=steplist[k-2]+columnList[max-2]-columnList[min-2]
+            steplist[k+3]=steplist[k-1]+columnList[max-1]-columnList[min-1]
+          }
+         }else{
+           if(min===0&&max===width){
+            steplist[k]=steplist[k-4]
+            steplist[k+1]=steplist[k-3]
+            steplist[k+2]=steplist[k-2]
+            steplist[k+3]=steplist[k-1]
+           }else if(min===0){
+            steplist[k]=steplist[k-4]+columnList[max-4]
+            steplist[k+1]=steplist[k-3]+columnList[max-3]
+            steplist[k+2]=steplist[k-2]+columnList[max-2]
+            steplist[k+3]=steplist[k-1]+columnList[max-1]
+          }else if(max===width){
+            steplist[k]=steplist[k-4]-columnList[min-4]
+            steplist[k+1]=steplist[k-3]-columnList[min-3]
+            steplist[k+2]=steplist[k-2]-columnList[min-2]
+            steplist[k+3]=steplist[k-1]-columnList[min-1]
+          }
+         }
+       }
+        let count=(max-min)*(endRow-startRow)/4
+        resultList[current] = (~~(steplist[k] / count))
+        resultList[current + 1] = (~~(steplist[k+1] / count))
+        resultList[current + 2] = (~~(steplist[k+2] / count))
+        resultList[current + 3] = (~~(steplist[k+3] / count))
+        current += 4
+        if(k===width-4){
+          if(endRow-startRow===(step/2)){
+              if(endRow===rowLen){
+              for(let n=0;n<width;n++){
+                let temp=n+startRow*width 
+                columnList[n]=columnList[n]-rgbList[temp]
+              }
+            }else{
+              for(let n1=0;n1<width;n1++){
+                columnList[n1]=columnList[n1]-rgbList[n1+startRow*width]+rgbList[n1+endRow*width]
+             
+              }
+            }
+          }else{
+            if(startRow===0&&endRow===rowLen){
+            }else if(startRow===0){
+              for(let n3=0;n3<width;n3++){
+                columnList[n3]=columnList[n3]+rgbList[n3+endRow*width]
+              }
+            }else if(endRow===rowLen){
+              for(let n2=0;n2<width;n2++){
+                columnList[n2]=columnList[n2]-rgbList[n2+startRow*width]
+              }
+            }
+          }
+        }
+      }
+    }
+
+
     date = new Date
     let time2 = date.getTime()
     console.log(`处理耗时${time2 - time1}毫秒`)
@@ -138,9 +249,10 @@ export default class HomePageComponent extends Component {
     console.log(`处理耗时${time2 - time1}毫秒`)
   }
   clickButton() {
-    let img:any=this.rgbList
-    for(let i=0;i<1;i++){
-      img=this.averageBulr(img,900,40)
+    let img: any = this.rgbList
+    console.log(img.data)
+    for (let i = 0; i < 1; i++) {
+      img = this.averageBulr1(img, 900, 100)
     }
     this.ctx.putImageData(img, 0, 0)
 
